2f152288d405a612c4e2cc492e3adf12ffc1928d
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-02-28 13:52:01 +0100

    restart it

---
+++ iocBoot/iocws3122/asyn_usbtmc.cmd
@@ -0,0 +1,15 @@
+
+epicsThreadSleep 2
+asynReport 2
+
+dbpf "$(P)$(R).OFMT" "ASCII"
+dbpf "$(P)$(R).IFMT" "Hybrid"
+dbpf "$(P)$(R).TMOD" "Write/Read"
+
+dbpf "$(P)$(R).AOUT" "*IDN?"
+epicsThreadSleep 2
+
+echo In another window run caget -S $(P)$(R).BINP and confirm that the IDN string was read.
+
+dbpr "$(P)$(R)" 1
+
